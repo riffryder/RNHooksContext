b8a64e4f0b6a7cf1992f5ea12ec53f13042755d9
Raphael Croce <rcroce7@gmail.com>
2019-05-23 16:22:34 -0500

    Add comments to custom hook and also implement return func to remove listener when component unmounts

--- customHook.js
+++ customHook.js
@@ -11,9 +11,18 @@ const setState = (newState) => {
 };
 
 const useCustom = () => {
+  //new listener will be equals to a function that's similar to this.setState
   const newListener = useState()[1];
+  console.log("new listener");
   useEffect(() => {
+    //called just after the component mounts
     listeners.push(newListener);
+    console.log("new listener added to array", listeners);
+    return () => {
+      //called just before the component unmounts
+      listeners = listeners.filter(listener => listener !== newListener)
+      console.log("listener removed from array", listeners)
+    }
   }, []);
 
   return [state, setState];
